60b4d73da889169e4c5ebd0e31e55b2caffc4750
Jenny Lee <112632530+klcheung1100@users.noreply.github.com>
2022-11-05 08:26:26 +0000

    Update test_convert_to_int.py

--- test_convert_to_int.py
+++ test_convert_to_int.py
@@ -1,7 +1,53 @@
 # Test Driven Development (TDD)
+
+# Normal arguments for convert_to_int() are integer strings with comma as thousands separators.
+#  Since the best practice is to test a function for two to three normal arguments, here are three examples with no comma, one comma and two commas respectively.
+# Example 1: Argument value - "756" Expected Return Value - 756
+# Example 2: Argument value - "2,081"   Expected Return Value - 2081
+# Example 3: Argument value - "1,034,891    Expect Return Value - 1034891
+
 import pytest
 
 
+# Test for normal arguments
+def test_with_no_comma():
+    actual = convert_to_int("756")
+    # Complete the assert statement
+    assert actual == 756, "Expected: 756, Actual: {0}".format(actual)
+
+
+def test_with_one_comma():
+    actual = convert_to_int("2,081")
+    # Complete the assert statement
+    assert actual == 2081, "Expected: 2081, Actual: {0}".format(actual)
+
+
+def test_with_two_commas():
+    actual = convert_to_int("1,034,891")
+    # Complete the assert statement
+    assert actual == 1034891, "Expected: 1034891, Actual: {0}".format(actual)
+
+
+# test for an argument with missing comma
+def test_on_string_with_missing_comma():
+    actual = convert_to_int("178100,301")
+    assert actual is None, "Expected: None, Actual: {0}".format(actual)
+
+
+def test_on_string_with_incorrectly_placed_comma():
+    # Assign to the actual return value for the argument "12,72,891"
+    actual = convert_to_int("12,72,891")
+    assert actual is None, "Expected: None, Actual: {0}".format(actual)
+
+
+# test for an argument with float value string
+def test_on_float_valued_string():
+    actual = convert_to_int("23,816.92")
+    # Complete the assert statement
+    assert actual is None, "Expected: None, Actual: {0}".format(actual)
+
+
+# test for an argument with integer sting with commas
 def convert_to_int(integer_string_with_commas):
     comma_separated_parts = integer_string_with_commas.split(",")
     for i in range(len(comma_separated_parts)):
